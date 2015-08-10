b2a785a3a8844a7430cefe4a58f225fac62218fe
Ferdinando Grande <grafer74@users.noreply.github.com>
2015-08-10 11:48:13 +0100

    Update romanNumberConverter.js

--- romanNumberConverter.js
+++ romanNumberConverter.js
@@ -1,3 +1,5 @@
+// With this script we can easily convert from arabic to roman numbers.
+
 function convert2Roman(num) {
 	if (num === 0)
     {
@@ -14,7 +16,7 @@ function convert2Roman(num) {
     }
   digits.push(num); // Here we assign Units.
   
-  // The first element (at position [0]) for each array is "", so we do not write any symbol if we do not need it after the convertion.
+  // The first element (at position [0]) for each array is "", so we do not write any symbol if we do not need it after the conversion.
     rUnits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]; 
     rTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
     rHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];

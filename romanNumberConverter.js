e3368ee81eb5cb84e3a5a26aafa856f7ff0dc039
Ferdinando Grande <grafer74@users.noreply.github.com>
2015-08-10 09:49:48 +0100

    Create romanNumberConverter.js

---
+++ romanNumberConverter.js
@@ -0,0 +1,33 @@
+function convert2Roman(num) {
+	if (num === 0)
+    {
+      return "ERROR! We cannot convert 0";
+    }
+ else if (num < 4000) // There is no universal way to write numbers >= 4000
+ { 
+  var digits = []; 
+  for (i=1000; i>9; i/=10) // 3 iterations: for Thousands, Hundreds, Tens
+    {
+      myFloor = Math.floor(num/i);
+      digits.push(myFloor);
+      num = num - (myFloor*i);
+    }
+  digits.push(num); // Here we assign Units.
+  
+    rUnits = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    rTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    rHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    rThousands = ["", "M", "MM", "MMM"];
+  
+  var romanNumber = rThousands[digits[0]] + rHundreds[digits[1]] + rTens[digits[2]] + rUnits[digits[3]]; // Finally we can build the Roman number string
+  
+  
+ return romanNumber;
+    }
+  else
+    {
+      return "ERROR! The number is too big!";
+    }
+}
+
+convert2Roman(123); // Here we can invoke the function, passing as a parameter the number we want to convert.
